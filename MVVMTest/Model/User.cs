d73050242a35812bdde11bd270ec1bc2b1fa1e03
Victor Fager <victorfager@gmail.com>
2019-11-10 21:59:51 +0100

    testat lite MVVM

---
+++ MVVMTest/Model/User.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMTest.Model
+{
+    public class User : INotifyPropertyChanged
+    {
+        private string userName;
+        private int port;
+
+        public string UserName 
+        {
+            get
+            {
+                return userName;
+            }
+
+            set 
+            {
+                userName = value;
+                RaisePropertyChanged("UserName");
+                RaisePropertyChanged("InfoString");
+            } 
+        }
+
+        public int Port 
+        { 
+            get
+            {
+                return port;
+            }
+            
+            set
+            {
+                port = value;
+                RaisePropertyChanged("Port");
+                RaisePropertyChanged("InfoString");
+            }
+        }
+
+        public string InfoString 
+        { 
+            get
+            {
+                return "User: " + userName + "  | Port: " + port.ToString();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
---
+++ MVVMTest/ViewModel/UserViewModel.cs
@@ -0,0 +1,35 @@
+﻿using MVVMTest.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMTest.ViewModel
+{
+    public class UserViewModel
+    {
+        public UserViewModel()
+        {
+            LoadUsers();
+        }
+
+        public ObservableCollection<User> Users
+        {
+            get;
+            set;
+        }
+
+        public void LoadUsers()
+        {
+            ObservableCollection<User> users = new ObservableCollection<User>();
+
+            users.Add(new User { UserName = "Victor", Port = 1030 });
+            users.Add(new User { UserName = "Moe", Port = 1040 });
+            users.Add(new User { UserName = "Rune", Port = 1337 });
+
+            Users = users;
+        }
+    }
+}
