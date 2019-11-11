561f0301dfdeb8ca193d78929ed35202c84c60eb
Mohammad <mohom373@student.liu.se>
2019-11-11 11:46:50 +0100

    Binding between View and ViewModel works

---
+++ P2PChat/P2PChatProj/Models/User.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2PChatProj.Model
+{
+    public class User : INotifyPropertyChanged
+    {
+        
+        private string userName;
+        private int portNumber;
+
+        public string UserName 
+        {
+            get
+            {
+                return userName;
+            }
+
+            set 
+            {
+                userName = value;
+                RaisePropertyChanged("UserName");
+                RaisePropertyChanged("InfoString");
+            } 
+        }
+
+        public int PortNumber 
+        { 
+            get
+            {
+                return portNumber;
+            }
+            
+            set
+            {
+                portNumber = value;
+                RaisePropertyChanged("PortNumber");
+                RaisePropertyChanged("InfoString");
+            }
+        }
+
+        public string InfoString 
+        { 
+            get
+            {
+                return "User: " + userName + "  | Port: " + portNumber.ToString();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
---
+++ P2PChat/P2PChatProj/ViewModels/UserConnectViewModel.cs
@@ -0,0 +1,27 @@
+﻿using P2PChatProj.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2PChatProj.ViewModel
+{
+    public class UserConnectViewModel
+    {
+        public UserConnectViewModel()
+        {
+            user = new User();
+        }
+
+        private User user;
+
+        public User User
+        {
+            get { return user; }
+
+            set { user = value; }
+        }
+    }
+}
