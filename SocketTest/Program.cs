4e2bae6482f4019e15264a2bd159123915d45eea
Victor Fager <victorfager@gmail.com>
2019-11-16 19:40:37 +0100

    Testing sockets

---
+++ SocketTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // LISTEN
+            int port = 8080;
+            IPHostEntry hostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ip = hostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ip, port);
+
+            Socket listener = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(localEndPoint);
+            listener.Listen(100);
+
+            Console.WriteLine("Listening to port " + port.ToString());
+
+            Socket handler = listener.Accept();
+
+            Console.WriteLine("Have gotten connection with " + handler.RemoteEndPoint.ToString());
+
+           
+            Console.ReadLine(); // Used to keep console running in the end
+        }
+    }
+}
