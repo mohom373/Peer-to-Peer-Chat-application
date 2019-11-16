07349a51df207ee8c3e83e5b790e42cbf8759343
Victor Fager <victorfager@gmail.com>
2019-11-16 20:56:13 +0100

    cont. with sockets

--- SocketTest/Program.cs
+++ SocketTest/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
 
@@ -13,22 +9,50 @@ namespace SocketTest
         static void Main(string[] args)
         {
             // LISTEN
-            int port = 8080;
-            IPHostEntry hostInfo = Dns.GetHostEntry(Dns.GetHostName());
-            IPAddress ip = hostInfo.AddressList[0];
-            IPEndPoint localEndPoint = new IPEndPoint(ip, port);
+            //int port = 8080;
+            //IPHostEntry hostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            //IPAddress ip = hostInfo.AddressList[0];
+            //IPEndPoint localEndPoint = new IPEndPoint(ip, port);
 
-            Socket listener = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            listener.Bind(localEndPoint);
-            listener.Listen(100);
+            //Socket listener = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            //listener.Bind(localEndPoint);
+            //listener.Listen(100);
 
-            Console.WriteLine("Listening to port " + port.ToString());
+            //Console.WriteLine("Listening to port " + port.ToString());
 
-            Socket handler = listener.Accept();
+            //Socket handler = listener.Accept();
 
-            Console.WriteLine("Have gotten connection with " + handler.RemoteEndPoint.ToString());
+            //Console.WriteLine("Have gotten connection with " + handler.RemoteEndPoint.ToString());
 
-           
+            // CONNECT
+            Socket connectSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            string ipString = "192.168.1.91";
+            IPAddress ip = IPAddress.Parse(ipString);
+            IPEndPoint ipep = new IPEndPoint(ip, 8080);
+
+            try
+            {
+                connectSocket.Connect(ipep);
+            }
+            catch (ArgumentNullException ae)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ae.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+
+            if (connectSocket.Connected)
+            {
+                Console.WriteLine("We are connected");
+            }
+
+
             Console.ReadLine(); // Used to keep console running in the end
         }
     }
