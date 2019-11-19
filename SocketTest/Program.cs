b15b8a2f9f336d83bd80f4f2f5012a9244e0b14e
Victor Fager <victorfager@gmail.com>
2019-11-19 10:54:03 +0100

    small changes

--- SocketTest/Program.cs
+++ SocketTest/Program.cs
@@ -9,26 +9,26 @@ namespace SocketTest
         static void Main(string[] args)
         {
             // LISTEN
-            int port = 8080;
-            IPHostEntry hostInfo = Dns.GetHostEntry(Dns.GetHostName());
-            IPAddress ip = hostInfo.AddressList[3];
-            IPEndPoint localEndPoint = new IPEndPoint(ip, port);
+            //int port = 8080;
+            //IPHostEntry hostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            //IPAddress ip = hostInfo.AddressList[2];
+            //IPEndPoint localEndPoint = new IPEndPoint(ip, port);
 
-            Socket listener = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            listener.Bind(localEndPoint);
-            listener.Listen(100);
+            //Socket listener = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            //listener.Bind(localEndPoint);
+            //listener.Listen(100);
 
-            Console.WriteLine("IP: " + ip.ToString());
-            Console.WriteLine("Listening to port " + port.ToString());
+            //Console.WriteLine("IP: " + ip.ToString());
+            //Console.WriteLine("Listening to port " + port.ToString());
 
-            Socket handler = listener.Accept();
+            //Socket handler = listener.Accept();
 
-            Console.WriteLine("Have gotten connection with " + handler.RemoteEndPoint.ToString());
+            //Console.WriteLine("Have gotten connection with " + handler.RemoteEndPoint.ToString());
 
 
             // CONNECT
             Socket connectSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            string ipString = "192.168.1.91";
+            string ipString = "10.253.244.1";
             IPAddress ip = IPAddress.Parse(ipString);
             IPEndPoint ipep = new IPEndPoint(ip, 8080);
 
