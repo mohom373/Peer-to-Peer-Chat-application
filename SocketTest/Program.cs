9768e55ba06a86a6d193790bcf7bc8ed0769bfe0
Victor Fager <victorfager@gmail.com>
2019-11-19 21:31:27 +0100

    cont. testing sockets

--- SocketTest/Program.cs
+++ SocketTest/Program.cs
@@ -6,12 +6,86 @@ namespace SocketTest
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            ///////////////////////// SETUP //////////////////////////
+            string userName;
+            int localPort;
+            
+            Console.WriteLine("Welcome to Socket Chat!\n");
+            
+            Console.Write("Enter a name: ");
+            userName = Console.ReadLine();
+
+            Console.Write("Enter a port: ");
+            localPort = Convert.ToInt32(Console.ReadLine());
+
+
+            IPHostEntry hostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress localIp = hostInfo.AddressList[0];
+
+            foreach (IPAddress address in hostInfo.AddressList) 
+            {
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIp = address;
+                    break;
+                }
+            }
+          
+            IPEndPoint localEndPoint = new IPEndPoint(localIp, localPort);
+
+            Socket listener = new Socket(localIp.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(localEndPoint);
+            listener.Listen(100);
+
+            Console.WriteLine("\nWelcome " + userName + "!");
+            Console.WriteLine("IP: " + localIp);
+            Console.WriteLine("Listening to port: " + localPort.ToString() + "\n");
+            ///////////////////////// SETUP END //////////////////////////
+
+            
+            
+            /////////////////////////// MENU ////////////////////////////
+            int selection;
+            bool isRunning = true;
+
+            while (isRunning) 
+            {
+                Console.WriteLine("====== Menu ======");
+                Console.WriteLine("0 - Send Request");
+                Console.WriteLine("1 - Accept Request");
+                Console.WriteLine("2 - Quit");
+                Console.WriteLine("====== Menu ======");
+
+                Console.Write("\nEnter menu selection: ");
+                selection = Convert.ToInt32(Console.ReadLine());
+
+                if (selection == 0)
+                {
+                    Console.WriteLine("Send Request Mode...");
+                } 
+                else if (selection == 1) 
+                {
+                    Console.WriteLine("Accept Request Mode...");
+                }
+                else if (selection == 2)
+                {
+                    isRunning = false;
+                }
+                else
+                {
+                    Console.WriteLine("ERROR: Wrong input!");
+                }
+            }
+            ///////////////////////// MENU END //////////////////////////
+
+
             // LISTEN
             //int port = 8080;
             //IPHostEntry hostInfo = Dns.GetHostEntry(Dns.GetHostName());
-            //IPAddress ip = hostInfo.AddressList[2];
+            //IPAddress ip = hostInfo.AddressList[1];
             //IPEndPoint localEndPoint = new IPEndPoint(ip, port);
 
             //Socket listener = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
@@ -27,34 +101,32 @@ namespace SocketTest
 
 
             // CONNECT
-            Socket connectSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            string ipString = "10.253.244.1";
-            IPAddress ip = IPAddress.Parse(ipString);
-            IPEndPoint ipep = new IPEndPoint(ip, 8080);
+            //Socket connectSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //string ipString = "10.253.244.1";
+            //IPAddress ip = IPAddress.Parse(ipString);
+            //IPEndPoint ipep = new IPEndPoint(ip, 8080);
 
-            try
-            {
-                connectSocket.Connect(ipep);
-            }
-            catch (ArgumentNullException ae)
-            {
-                Console.WriteLine("ArgumentNullException : {0}", ae.ToString());
-            }
-            catch (SocketException se)
-            {
-                Console.WriteLine("SocketException : {0}", se.ToString());
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Unexpected exception : {0}", e.ToString());
-            }
+            //try
+            //{
+            //    connectSocket.Connect(ipep);
+            //}
+            //catch (ArgumentNullException ae)
+            //{
+            //    Console.WriteLine("ArgumentNullException : {0}", ae.ToString());
+            //}
+            //catch (SocketException se)
+            //{
+            //    Console.WriteLine("SocketException : {0}", se.ToString());
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            //}
 
-            if (connectSocket.Connected)
-            {
-                Console.WriteLine("We are connected");
-            }
-
-            Console.ReadLine(); // Used to keep console running in the end
+            //if (connectSocket.Connected)
+            //{
+            //    Console.WriteLine("We are connected");
+            //}
         }
     }
 }
